fcba32c3cbbdef5038448d8988f3413be3be0bcf
AnthonyBlackJ <1303524991@qq.com>
2019-07-28 09:12:02 +0800

    3

--- 20.py
+++ 20.py
@@ -3,6 +3,14 @@
 	例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。 
 	但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。'''
 class Solution:
+	def isNumeric1(self, s):
+	# 骚操作:将字符串转换成浮点数
+		try:
+			ss = float(s)
+			return True
+		except:
+			return False
+	# 也可以使用re的正则表达式来匹配
 	def isNumeric(self, s):
 		self.s = s
 		self.l = len(s)
@@ -12,7 +20,7 @@ class Solution:
 
 	def isNumericCore(self, i):
 		numeric = self.scanInteger(i)[0]
-		j = self.scanUnsignInteger(i)[1]
+		j = self.scanInteger(i)[1]
 		if j < self.l and self.s[j] == '.':
 			j += 1
 			# 0.123 123.都对，即.前后只要有一边有数字即可
@@ -26,7 +34,7 @@ class Solution:
 		return numeric and self.l == j
 
 	def scanInteger(self, index):
-		if self.s[index] in {'+', '-'}:
+		if index < self.l and self.s[index] in {'+', '-'}:
 			index += 1
 		return self.scanUnsignInteger(index)
 
@@ -36,4 +44,4 @@ class Solution:
 			index += 1
 		return (bool(index>mark), index)
 s = Solution()
-print(s.isNumeric('+500'))
+print(s.isNumeric('12e'))
